e9343b29433adcdf3c1cc69aab6b161716499ac7
HarshakumarGH <harshakumargh@gmail.com>
2019-08-27 12:36:34 +0530

    5 files modified

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-// import {QuizQuestionsService} from '../../../shared/quiz-questions.service';
+import {QuestionService} from '../../_services/question.service';
 // import {QuizQuestion} from './quiz-question.model';
 
 @Component({
@@ -8,6 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quiz.component.scss']
 })
 export class QuizComponent implements OnInit {
-  ngOnInit() {
+    correctCount = 0;
+  currentQuestion = 0;
+  progressValue = 0;
+  questionCount;
+  questions$;
+  selectedOption;
+  CONGRATULATIONS = 'assets/img/congratulations.jpg';
+  TRY_AGAIN = 'assets/img/try-again.png';
+  constructor(private quizQuestionsService: QuestionService) {
+  }
+ ngOnInit() {
+    this.questions$ = this.quizQuestionsService.getQuestions();
+    this.questions$.subscribe((questions) => {
+        this.questionCount = questions['quiz-questions'].length;
+        this.progressValue = 100 * (this.currentQuestion + 1) / this.questionCount;
+    });
+  }
+
+  nextQuestion(answer: number) {
+    if (answer === this.selectedOption) {this.correctCount++; }
+    delete this.selectedOption;
+    this.currentQuestion++;
+    this.progressValue = 100 * (this.currentQuestion + 1) / this.questionCount;
   }
 }
